80d9348fbfb8b4ce3d63c277c53a30a14cef63a3
majerski <tomasz.majerski@o2.pl>
2014-05-23 22:03:49 +0200

    Made in House #3

--- js/index.js
+++ js/index.js
@@ -1,50 +1,29 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    SOME_CONSTANTS : false,  // some constant
-
-
-    // Application Constructor
     initialize: function() {
-        console.log("console log init");
         this.bindEvents();
         this.initFastClick();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener("offline", this.onOffline, false);
+		document.addEventListener("online", this.onOnline, false);
+		document.addEventListener("load", this.onLoad, false);
     },
-    initFastClick : function() {
+    initFastClick: function() {
         window.addEventListener('load', function() {
             FastClick.attach(document.body);
-        }, false);
+        },false);
     },
-    // Phonegap is now ready...
     onDeviceReady: function() {
-        window.plugins.toast.showShortTop('Hello there!',function(a){
-			console.log('toast success: ' + a)
-		},
-		function(b){
-			alert('toast error: ' + b)}
-		);
+		
+    },
+	onOffline: function() {
+        window.plugins.toast.showLongBottom('Brak połączenia z internetem.');
+    },
+	onOnline: function() {
+        // google map
+    }
+	onLoad: function() {
+        
     }
 };
