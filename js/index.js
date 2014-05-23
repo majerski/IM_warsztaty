0e8fec1a0d32df5ab3838f4f69c5fbd08f007d98
majerski <tomasz.majerski@o2.pl>
2014-05-23 22:57:38 +0200

    Made in House #4.0.1
    File Manager test

--- js/index.js
+++ js/index.js
@@ -19,9 +19,21 @@ var app = {
     },
 	onOffline: function() {
 		window.plugins.toast.showLongBottom('Brak połączenia z internetem.',function(a){},function(b){});
+		// dodać ikonę o braku internetu + odśwież
 	},
 	onOnline: function() {
-        // google map
+        // google map + RSS
+		var FPreturnSuccess='FPsuccess';
+		var FPSettingsFileName='camera-2.txt';
+		var FPSettingsDownloadUrl='http://www.tvregionalna24.pl/camera-2.txt';
+		var FPbase='http://www.tvregionalna24.pl/';
+		var FPsuccess = function(result) { 
+			alert("SUCCESS: \r\n"+result );    
+		};
+		var FPerror = function(error) { 
+			alert("ERROR: \r\n"+error ); 
+		};
+		FilePlugin.callNativeFunction(FPsuccess,FPerror,{'result':FPreturnSuccess,'file':FPSettingsFileName,'downloadurl':FPSettingsDownloadUrl,'base_path':FPbase}); 
     },
 	onLoad: function() {
         
