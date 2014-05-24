afa23925764a8c2b04e12d616b05d1ca26aa5b65
majerski <tomasz.majerski@o2.pl>
2014-05-24 12:08:03 +0200

    Made in House #5.1

--- js/index.js
+++ js/index.js
@@ -19,9 +19,21 @@ var app = {
     },
 	onOffline: function() {
 		window.plugins.toast.showShortCenter('Brak połączenia z internetem.',function(a){},function(b){});
+		var warsztaty = [
+			{"nazwa":"nazwa1","adres":"adres1","tel":"tel1","lat":54.156524,"lng":19.410400},
+			{"nazwa":"nazwa2","adres":"adres2","tel":"tel2","lat":54.153710,"lng":19.389114},
+			{"nazwa":"nazwa3","adres":"adres3","tel":"tel3","lat":54.139231,"lng":19.441986},
+		];
 	},
 	onOnline: function() {
-		
+		var request = $.ajax({
+			url: "http://www.tvregionalna24.pl/warsztaty.php",
+			type: "GET",
+			dataType: "json"
+		});
+		request.done(function(response){
+			alert(response);
+		});
     },
 	onLoad: function() {
         
