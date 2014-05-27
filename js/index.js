4c6d4f9de5b70e543e3979a8396ad131fb171b0a
majerski <tomasz.majerski@o2.pl>
2014-05-27 15:38:45 +0200

    .

--- js/index.js
+++ js/index.js
@@ -13,28 +13,40 @@ var app = {
     },
     onDeviceReady: function() {
 		document.addEventListener("load", this.onLoad, false);
-		document.addEventListener("offline", this.onOffline, false);
-		document.addEventListener("online", this.onOnline, false);
+		//document.addEventListener("offline", this.onOffline, false);
+		//document.addEventListener("online", this.onOnline, false);
+		this.checkConnection();
     },
 	onLoad: function() {
         alert("onLoad");
     },
+	checkConnection: function() {
+		var networkState = navigator.connection.type;
+
+		var states = {};
+		states[Connection.UNKNOWN]  = 'Unknown connection';
+		states[Connection.ETHERNET] = 'Ethernet connection';
+		states[Connection.WIFI]     = 'WiFi connection';
+		states[Connection.CELL_2G]  = 'Cell 2G connection';
+		states[Connection.CELL_3G]  = 'Cell 3G connection';
+		states[Connection.CELL_4G]  = 'Cell 4G connection';
+		states[Connection.CELL]     = 'Cell generic connection';
+		states[Connection.NONE]     = 'No network connection';
+
+		alert('Connection type: ' + states[networkState]);
+	}/*,
 	onOffline: function() {
-		alert("onOffline");
 		//window.plugins.toast.showShortCenter('Brak połączenia z internetem.',function(a){},function(b){});
 	},
 	onOnline: function() {
-		alert("onOnline");
-		/*
-		var RSS = $.ajax({
-			url: "http://www.q-service.com.pl/rss/",
-			type: "GET",
-			dataType: "json",
-			async: false
-		});
-		RSS.done(function(response){
-			alert(response);
-		});
-		*/
-    }
+		//var RSS = $.ajax({
+		//	url: "http://www.q-service.com.pl/rss/",
+		//	type: "GET",
+		//	dataType: "json",
+		//	async: false
+		//});
+		//RSS.done(function(response){
+		//	alert(response);
+		//});
+    }*/
 };
